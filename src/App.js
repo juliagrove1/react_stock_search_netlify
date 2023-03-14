6b2399cb93a21fa181c18143e79e13a760b44bb7
Julia Grove <lauragrove@Julias-MBP-5.attlocal.net>
2023-03-14 13:44:45 -0400

    stocks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import axios, { Axios } from 'axios'
 import './App.css';
-
 function App() {
+
+
+  const [data,setData] = useState({})
+  const [sym, setSymbol] = useState('')
+
+  const URL = `https://finnhub.io/api/v1/quote?symbol=${sym}&token=cg781v1r01qus5fl0orgcg781v1r01qus5fl0os0`
+
+  const searchSymbol = (Event) => {
+    if (Event.key === 'Enter') {
+
+      axios.get(URL).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+        })
+       
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=DM+Sans&family=Josefin+Sans:wght@500;600&family=Poppins:wght@200&display=swap');
+      </style>
+
+      <div className = "search">
+        <input
+          value={sym}
+          onChange={Event => setSymbol(Event.target.value)}
+          onKeyPress={searchSymbol}
+          placeholder = "Stock Symbol"
+          type="text"/>
+      </div>
+
+      {data.c === undefined && 
+      <div className="container">
+        <div className="welcome">
+            <p>Type in a stock symbol and hit enter to retrieve data</p>
+        </div>
+      </div>
+      }
+
+      {data.c !== undefined &&
+        <div className="container"> 
+        <div className="topLeft">
+          <div className="symbol">
+           
+          </div>
+          <div className="qAndDaily">
+              <div className="quote">
+                  <h1>{data.c}</h1>
+             </div>
+            <div className="priceChange">
+              <div className ="negChange">
+                {data.d < 0 &&
+                  <h1>{data.d} ({data.dp}%)</h1>
+                }
+              </div>
+              <div className="posChange">
+                {data.d > 0 &&
+                  <h1>+{data.d} ({data.dp}%)</h1>
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+          <div className="topRight">
+            <div className="openClose">
+              <h4>prev close: {data.pc}</h4>
+              <h4>open: {data.o}</h4>
+            </div>
+
+            <div className="highLow">
+              <h4>todays high: {data.h}</h4>
+              <h4>todays low: {data.l}</h4>
+              </div>
+          </div>
+        </div>
+      }
+      
     </div>
   );
 }
