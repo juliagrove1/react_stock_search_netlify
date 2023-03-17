3ffe1d0273b4b3b2148bfed5b852586994b96521
juliagrove1 <112003183+juliagrove1@users.noreply.github.com>
2023-03-16 20:40:27 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,40 +1,47 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import './App.css';
+import News from './news'
+
 function App() {
-
-
+  
   const [data,setData] = useState({})
   const [sym, setSymbol] = useState('')
-
+  const [printSym, setPrint] = useState('')
+  
   const URL = `https://finnhub.io/api/v1/quote?symbol=${sym}&token=cg781v1r01qus5fl0orgcg781v1r01qus5fl0os0`
+ 
+  const handleSubmit=(e)=> {
+    e.preventDefault();
 
-  const searchSymbol = (Event) => {
-    if (Event.key === 'Enter') {
+    axios.get(URL).then((response) => {
+      setData(response.data)
+      console.log(response.data)
+      })
+      .catch((err) => console.log(err));
+      setPrint(sym);
 
-      axios.get(URL).then((response) => {
-        setData(response.data)
-        console.log(response.data)
-        })
-       
     }
-  }
 
   return (
     <div className="app">
-     
+      
       <style>
         @import url('https://fonts.googleapis.com/css2?family=DM+Sans&family=Josefin+Sans:wght@500;600&family=Poppins:wght@200&display=swap');
       </style>
 
-      <div className = "search">
-        <input
-          value={sym}
-          onChange={Event => setSymbol(Event.target.value)}
-          onKeyPress={searchSymbol}
-          placeholder = "Stock Symbol (AAPL)"
-          type="text"/>
-      </div>
+      <form onSubmit={handleSubmit}>
+          <div className = "search">
+            <input
+              type="text"
+              name="sym"
+              placeholder="enter symbol"
+              onChange={(e)=> {
+                setSymbol(e.target.value);}}/>
+              <button>Enter</button>
+          </div>
+          
+      </form>
 
       {data.c === undefined && 
       <div className="container">
@@ -48,7 +55,7 @@ function App() {
         <div className="container"> 
         <div className="topLeft">
           <div className="symbol">
-           
+           <p>{printSym.toUpperCase()}</p>
           </div>
           <div className="qAndDaily">
               <div className="quote">
@@ -67,6 +74,9 @@ function App() {
               </div>
             </div>
           </div>
+               
+           
+
         </div>
           <div className="topRight">
             <div className="openClose">
@@ -79,9 +89,10 @@ function App() {
               <h4>todays low: {data.l}</h4>
               </div>
           </div>
+
         </div>
-      }
-      
+              }
+            
     </div>
   );
 }
