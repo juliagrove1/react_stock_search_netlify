727364928c95e61747b9bef2a16d114eae88df78
juliagrove1 <112003183+juliagrove1@users.noreply.github.com>
2023-03-21 14:11:21 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,12 +8,6 @@ function App() {
   const [data,setData] = useState({})
   const [sym, setSymbol] = useState('')
   const [printSym, setPrint] = useState('')
-  const [news, setNews] = useState([])
-  const [news2, setNews2] = useState([])
-  const [news3, setNews3] = useState([])
-  const [Img1, setImg1] = useState([])
-  const [Img2, setImg2] = useState([])
-  const [Img3, setImg3] = useState([])
   const [newsUrl, setNewsUrl] = useState([])
   const [newsUrl2, setNewsUrl2] = useState([])
   const [newsUrl3, setNewsUrl3] = useState([])
@@ -25,28 +19,9 @@ function App() {
   const [symNewsUrl2, setSymNewsUrl2] = useState([])
 
   const URL = `https://finnhub.io/api/v1/quote?symbol=${sym}&token=cg781v1r01qus5fl0orgcg781v1r01qus5fl0os0`
-  const NEWS_URL = `https://finnhub.io/api/v1/news?category=general&token=cg781v1r01qus5fl0orgcg781v1r01qus5fl0os0`
   const SYM_NEWS = `https://finnhub.io/api/v1/company-news?symbol=${sym}&from=2023-01-01&to=2023-03-19&token=cg781v1r01qus5fl0orgcg781v1r01qus5fl0os0`
 
-  const fetchNewsData = useCallback(() => {
-    return axios.get(NEWS_URL)
-      .then(response => {
-        setImg1(response.data[0].image)
-        setImg2(response.data[1].image)
-        setImg3(response.data[2].image)
-        setNewsUrl(response.data[0].url)
-        setNewsUrl2(response.data[1].url)
-        setNewsUrl3(response.data[2].url)
-        setNews(response.data)
-        setNews2(response.data[1].headline)
-        setNews3(response.data[2].headline)
-        console.log(response.data[0].headline)
-        return response.data[0].headline;
-      })
-      .catch((err) => {
-        console.error(err);
-      })
-  }, [fetchNewsData])
+  
   const fetchCompanyNews = async () => {
     return axios.get(SYM_NEWS)
       .then(response => {
@@ -67,12 +42,6 @@ function App() {
   }
   // use effect loads on page load 
 
-  useEffect(() => {
-    fetchNewsData().then(result => {
-      setNews(result || '');
-    })
-  }, [fetchNewsData])
-
   const handleSubmit=(e)=> {
     e.preventDefault();
     axios.get(URL).then((response) => {
@@ -87,6 +56,7 @@ function App() {
   return (
     
     <div className="app">
+    
       <style>
         @import url('https://fonts.googleapis.com/css2?family=DM+Sans&family=Josefin+Sans:wght@500;600&family=Poppins:wght@200&display=swap');
       </style>
@@ -103,26 +73,6 @@ function App() {
           </div>
       </form>
     <div className="seperate">
-
-      <div className="marketNews">
-          <p className="header">Market News</p>
-              <div className="newslinks">
-                <div className = "article1">
-                  <img src={Img1} alt=" "></img>
-                  <a href={newsUrl}>{news}</a>
-                </div>
-
-                <div className = "article2">
-                  <img src={Img2} alt=" "></img>
-                  <a href={newsUrl2}>{news2}</a>
-                </div>
-
-                <div className = "article3">
-                  <img src={Img3} alt=" "></img>
-                  <a href={newsUrl3}>{news3}</a>
-                </div>
-              </div>
-        </div>
 
       {data.c === undefined && 
       <div className="container">
@@ -170,12 +120,12 @@ function App() {
           <div className="newsheadline">
           <p>Headlines:</p>
              <div className="symbolnews1">
-                <img src={symNewsImg} alt=" "></img>
+                <img src={symNewsImg}></img>
                   <a href={symNewsUrl}>{symNews}</a>
              </div>
 
              <div className="symbolnews1">
-                <img src={symNewsImg2} alt=" "></img>
+                <img src={symNewsImg2}></img>
                   <a href={symNewsUrl2}>{symNews2}</a>
              </div>
 
