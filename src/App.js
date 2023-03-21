df52b5d4e1ac36635b050594805074548095aa66
juliagrove1 <112003183+juliagrove1@users.noreply.github.com>
2023-03-21 13:18:07 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,27 +1,89 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import './App.css';
+
 
 function App() {
   
   const [data,setData] = useState({})
   const [sym, setSymbol] = useState('')
   const [printSym, setPrint] = useState('')
-  
+  const [news, setNews] = useState([])
+  const [news2, setNews2] = useState([])
+  const [news3, setNews3] = useState([])
+  const [Img1, setImg1] = useState([])
+  const [Img2, setImg2] = useState([])
+  const [Img3, setImg3] = useState([])
+  const [newsUrl, setNewsUrl] = useState([])
+  const [newsUrl2, setNewsUrl2] = useState([])
+  const [newsUrl3, setNewsUrl3] = useState([])
+  const [symNews, setSymNews] = useState([])
+  const [symNewsImg, setSymNewsImg] = useState([])
+  const [symNewsUrl, setSymNewsUrl] = useState([])
+  const [symNews2, setSymNews2] = useState([])
+  const [symNewsImg2, setSymNewsImg2] = useState([])
+  const [symNewsUrl2, setSymNewsUrl2] = useState([])
+
   const URL = `https://finnhub.io/api/v1/quote?symbol=${sym}&token=cg781v1r01qus5fl0orgcg781v1r01qus5fl0os0`
- 
+  const NEWS_URL = `https://finnhub.io/api/v1/news?category=general&token=cg781v1r01qus5fl0orgcg781v1r01qus5fl0os0`
+  const SYM_NEWS = `https://finnhub.io/api/v1/company-news?symbol=${sym}&from=2023-01-01&to=2023-03-19&token=cg781v1r01qus5fl0orgcg781v1r01qus5fl0os0`
+
+  const fetchNewsData = () => {
+    return axios.get(NEWS_URL)
+      .then(response => {
+        setImg1(response.data[0].image)
+        setImg2(response.data[1].image)
+        setImg3(response.data[2].image)
+        setNewsUrl(response.data[0].url)
+        setNewsUrl2(response.data[1].url)
+        setNewsUrl3(response.data[2].url)
+        setNews(response.data)
+        setNews2(response.data[1].headline)
+        setNews3(response.data[2].headline)
+        console.log(response.data[0].headline)
+        return response.data[0].headline;
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  }
+  const fetchCompanyNews = async () => {
+    return axios.get(SYM_NEWS)
+      .then(response => {
+        setSymNews(response.data[0].headline)
+        setSymNewsImg(response.data[0].image)
+        setSymNewsUrl(response.data[0].url)
+
+        setSymNews2(response.data[1].headline)
+        setSymNewsImg2(response.data[1].image)
+        setSymNewsUrl2(response.data[1].url)
+
+        console.log(response.data[0].headline)
+        return response.data[0].headline;
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  }
+  // use effect loads on page load 
+  // empty array only reveals one piece of data?
+  useEffect(() => {
+    fetchNewsData().then(result => {
+      setNews(result || '');
+    })
+  }, [])
+
   const handleSubmit=(e)=> {
     e.preventDefault();
-
     axios.get(URL).then((response) => {
       setData(response.data)
       console.log(response.data)
       })
       .catch((err) => console.log(err));
       setPrint(sym);
-
+      fetchCompanyNews();
+     
     }
-
   return (
     <div className="app">
       
@@ -39,13 +101,33 @@ function App() {
                 setSymbol(e.target.value);}}/>
               <button>Enter</button>
           </div>
-          
       </form>
+    <div className="seperate">
+
+      <div className="marketNews">
+          <p className="header">Market News</p>
+              <div className="newslinks">
+                <div className = "article1">
+                  <img src={Img1}></img>
+                  <a href={newsUrl}>{news}</a>
+                </div>
+
+                <div className = "article2">
+                  <img src={Img2}></img>
+                  <a href={newsUrl2}>{news2}</a>
+                </div>
+
+                <div className = "article3">
+                  <img src={Img3} alt=" "></img>
+                  <a href={newsUrl3}>{news3}</a>
+                </div>
+              </div>
+        </div>
 
       {data.c === undefined && 
       <div className="container">
         <div className="welcome">
-            <p>Type in a stock symbol and hit enter to retrieve data</p>
+            <p>Enter a stock symbol to retrieve data</p>
         </div>
       </div>
       }
@@ -72,10 +154,7 @@ function App() {
                 }
               </div>
             </div>
-          </div>
-               
-           
-
+          </div>  
         </div>
           <div className="topRight">
             <div className="openClose">
@@ -88,10 +167,26 @@ function App() {
               <h4>todays low: {data.l}</h4>
               </div>
           </div>
+          <div className="newsheadline">
+          <p>Headlines:</p>
+             <div className="symbolnews1">
+                <img src={symNewsImg}></img>
+                  <a href={symNewsUrl}>{symNews}</a>
+             </div>
 
+             <div className="symbolnews1">
+                <img src={symNewsImg2}></img>
+                  <a href={symNewsUrl2}>{symNews2}</a>
+             </div>
+
+             </div>
         </div>
+
+
+        
               }
-            
+        </div>
+        
     </div>
   );
 }
